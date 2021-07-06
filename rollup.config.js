3f3737b476e0c3750a94581a344ee924e49e4d85
Andrew Hyndman <ajhyndman@hotmail.com>
2021-07-06 03:00:04 -0700

    Build ESM target with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs',
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    plugins: [typescript()],
   },
-  plugins: [typescript()],
-};
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+    plugins: [typescript()],
+  },
+];
