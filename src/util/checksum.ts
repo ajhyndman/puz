5f02ddd2a3f92f9bd97a581988aab9763a32e527
Andrew Hyndman <ajhyndman@hotmail.com>
2021-07-19 22:59:24 -0700

    Edit checksum algorithm for rreadability

--- src/util/checksum.ts
+++ src/util/checksum.ts
@@ -1,10 +1,3 @@
-// 16-bit bit masks
-const enum BIT_MASK {
-  LEAST_SIGNIFICANT_BIT = 0x0001, // 0000 0000 0000 0001
-  ALL = 0xffff, // 1111 1111 1111 1111
-  MOST_SIGNIFICANT_BIT = 0x8000, // 1000 0000 0000 0000
-}
-
 /**
  * Implementation of the PUZ file checksum algorithm.
  *
@@ -16,14 +9,12 @@ const enum BIT_MASK {
  */
 export function checksum(data: Uint8Array, initialValue: number = 0x0000) {
   return data.reduce((sum, byte) => {
-    // rotate sum bits to the right
-    const leastSignificantBit = sum & BIT_MASK.LEAST_SIGNIFICANT_BIT;
+    // rotate checksum bits one position to the right
+    const leastSignificantBit = sum & 0x0001;
     sum = sum >>> 1;
-    if (leastSignificantBit) {
-      sum = (sum | BIT_MASK.MOST_SIGNIFICANT_BIT) & BIT_MASK.ALL;
-    }
+    sum = sum | (leastSignificantBit << 15);
 
-    // add next byte of data and re-apply mask
-    return (sum + byte) & BIT_MASK.ALL;
+    // add next byte of data and mask to 16 bits
+    return (sum + byte) & 0xffff;
   }, initialValue);
 }
